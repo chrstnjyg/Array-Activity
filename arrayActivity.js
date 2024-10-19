ac3cad58a9f534997f999eea23485bdbe3a3a867
Christine <guerrerochristinejoy15@gmail.com>
2024-10-20 04:31:28 +0800

    Initial commit with arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,15 @@
+// Create an array favSingers and store your three favorite singers.
+let favSingers = ["Avril Lavigne", "Taylor Swift", "Ariana Grande"];
+// Log the first singer using an array index notation.
+console.log(favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers.
+let favNumbers = ["1", "15", "22", "18"];
+
+// Create an array named mixedArr and store different data types.
+let mixedArr = ["string", ["otherarray"], 123, true ];
+// Log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
